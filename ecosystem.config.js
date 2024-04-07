c9c7bf312bf3dde5349937719f7472bb68e17549
jack6203 <joijoijoi2001@gmail.com>
2024-04-07 11:09:30 +0800

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'node-browserless', // application name 
+      script: 'index.js', // script path to pm2 start
+      instances: 1, // number process of application
+      autorestart: true, //auto restart if app crashes
+      watch: false,
+      max_memory_restart: '32G', // restart if it exceeds the amount of memory specified
+    }
+  ],
+};
